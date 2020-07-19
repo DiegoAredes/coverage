b1e61ebe10a7ab969f56a6446d566da30ebbfe5d
Diego Aredes <diego.aredes@globallogic.com>
2020-07-18 23:01:27 -0300

    test

---
+++ spring.coverage-example/src/test/java/com/globallogic/bootcamp/spring/coverageexample/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.globallogic.bootcamp.spring.coverageexample;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
